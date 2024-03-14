6aa5ab71e0370f5424bc6cae822c5cfdcd4d1639
YoussefAoun <y.aoun@outlook.com>
2024-03-14 15:21:42 +0200

    added status, tested apis for previous events, added paging to avoid overload data for events and users, added Json ignore for data that we do not need

--- Attend-It/sql-scripts/creating-entities/insert-events.sql
+++ Attend-It/sql-scripts/creating-entities/insert-events.sql
@@ -22,7 +22,7 @@ VALUES ('Art Exhibition', '2024-09-15', '10:00', 'Art Gallery', 'Explore a diver
 
 
 INSERT INTO Event (title, date, time, location, description, organizer_id)
-VALUES ('Testing', '2024-03-14', '08:30', 'Testing', 'Get motivated and learn new fitness techniques in this interactive workshop led by certified trainers', 1)
+VALUES ('Testing', '2024-03-14', '08:30', 'Testing', 'Get motivated and learn new fitness techniques in this interactive workshop led by certified trainers', 1);
 
 INSERT INTO Event (title, date, time, location, description, organizer_id)
 VALUES ('Testing past', '2024-03-13', '08:30', 'Testing', 'Get motivated and learn new fitness techniques in this interactive workshop led by certified trainers', 1)
